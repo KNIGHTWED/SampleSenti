06a8029f43d0fa388f394506b6a8f07bf2cb2c5d
KJM <jungmin0322@gmail.com>
2021-11-02 18:23:43 +0900

    Initial commit

--- app/src/main/java/com/example/samplesenti/MainActivity.java
+++ app/src/main/java/com/example/samplesenti/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.samplesenti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    RelativeLayout RelativeLayout_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_Login = findViewById(R.id.RelativeLayout_Login);
+
+        // 1. 값을 가져온다.
+        // 2. 클릭을 감지한다.
+        // 3. 1번의 값을 다음 액티비티로 넘긴다.
+
+
+        // 클릭 가능한 상태로 만들기
+        RelativeLayout_Login.setClickable(true);
+        RelativeLayout_Login.setOnClickListener(new View.OnClickListener() {
+            @Override // 클릭하면 실행되는 것.
+            public void onClick(View v) {
+                String email = TextInputEditText_email.getText().toString();
+                String password = TextInputEditText_password.getText().toString();
+
+                // 클래스에서 클래스로 이동 ( 출발 액티비티 이름을 명시해줘야함, 도착 액티비티는 .java 말고 .class 사용)
+                Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
